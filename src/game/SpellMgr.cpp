07259044b267beeba1a66612f384a5ef2153b477
Carl Hjerpe <lillecarl@gmail.com>
2012-08-11 21:43:47 +0200

    [00001] Fixing diminishing return for warlock.
    (cherry picked from commit 036a37287a12893b04bc8b6104ee334bf802b113)

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -3824,7 +3824,7 @@ DiminishingGroup GetDiminishingReturnsGroupForSpell(SpellEntry const* spellproto
         {
             // Fear
             if (spellproto->IsFitToFamilyMask(UI64LIT(0x40840000000)))
-                return DIMINISHING_WARLOCK_FEAR;
+                return DIMINISHING_FEAR;
             // Curses/etc
             if (spellproto->IsFitToFamilyMask(UI64LIT(0x00080000000)))
                 return DIMINISHING_LIMITONLY;
